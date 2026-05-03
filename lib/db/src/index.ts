2509c269051b2aa84b1a0e23b3ade66885122b08
madwy5275-sketch <madwy5275@gmail.com>
2026-05-03 04:49:08 +0300

    Upload Files

--- lib/db/src/index.ts
+++ lib/db/src/index.ts
@@ -14,5 +14,3 @@ export const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 export const db = drizzle(pool, { schema });
 
 export * from "./schema";
-
-export { runMigrations } from "./migrate";
